32435b6614518b4f8e3a45701750042a7956a31b
Md. Tajbir Hasan Shuvo <138506869+tajshuvoo@users.noreply.github.com>
2026-04-14 23:31:28 +0600

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        hash={}
+        for char in s:
+            hash[char]= hash.get(char,0)+1
+        count=0
+
+        for key, val in hash.items():
+            if val==1:
+                count+=1
+        
+        return True if count==1 or count==0 or len(s)==2 else False
