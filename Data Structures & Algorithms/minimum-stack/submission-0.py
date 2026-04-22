1e275b6f1e52ecc8fe85af33f14fb0730d9eff84
Md. Tajbir Hasan Shuvo <138506869+tajshuvoo@users.noreply.github.com>
2026-04-22 23:32:07 +0600

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,16 @@
+class MinStack:
+
+    def __init__(self):
+        self.s=[]
+
+    def push(self, val: int) -> None:
+        self.s.append(val)
+
+    def pop(self) -> None:
+        return self.s.pop()
+
+    def top(self) -> int:
+        return self.s[-1]
+
+    def getMin(self) -> int:
+        return min(self.s)
